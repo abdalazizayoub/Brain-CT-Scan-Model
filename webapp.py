77ea4d6eeed152854715da6f61c4e4586518aa21
Abdalaziz Ayoub <168747192+abdalazizayoub@users.noreply.github.com>
2025-04-10 15:00:25 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,44 +6,13 @@ from PIL import Image
 from torch.nn.functional import softmax
 
 
-class MyCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=256, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding="same"),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding="same"),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avg = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.25),
-            nn.Linear(64 * 6 * 6, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 2)
-        )
-
-    def forward(self, x):
-        x = self.layers(x)
-        x = self.avg(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
 
 
-model = torch.load("Model.pth", map_location=torch.device('cpu'),weights_only=False)
+model = torch.load("resnet.pth", map_location=torch.device('cpu'),weights_only=False)
 model.eval()
 
 t = torchvision.transforms.Compose([
     torchvision.transforms.PILToTensor(),
-    torchvision.transforms.Grayscale(3),
     torchvision.transforms.ConvertImageDtype(torch.float),
     torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     torchvision.transforms.Resize((64, 64)),
